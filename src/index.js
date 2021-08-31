e69c64e866ae9bd4afc0a490eb7b602fcdfc07d2
AlexHamn <agalexandro16@gmail.com>
2021-08-31 17:06:04 -0500

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import './style.css';
 
 const newTaskForm = document.querySelector('[data-new-task-form]');
@@ -7,78 +6,71 @@ const tasksList = document.getElementById('tasks');
 const LOCAL_STORAGE_TASKS_KEY = 'tasks.list';
 let tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASKS_KEY)) || [];
 
-function clearElement(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    }
-    console.log('done')
+function createTask(task) {
+  return { id: Date.now().toString(), task, complete: false };
 }
 
-const taskInput = document.getElementById('addToList');
-taskInput.value = null;
-
-newTaskForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const task = taskInput.value;
-    if (task == null || task === '') return;
-    const newTask = createTask(task);
-    tasks.push(newTask);
-    taskInput.value = null;
-    console.log(tasks);
-    renderTasks();
-})
-
-tasksList.addEventListener('click', e => {
-    const selectedTask = tasks.find(task => task.id === e.target.id)
-    selectedTask.complete = e.target.checked;
-    save();
-})
-
-function createTask(task) {
-    return {id: Date.now().toString(), task: task, complete: false}
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
 }
 
 function save() {
-    localStorage.setItem(LOCAL_STORAGE_TASKS_KEY, JSON.stringify(tasks));
+  localStorage.setItem(LOCAL_STORAGE_TASKS_KEY, JSON.stringify(tasks));
 }
 
 const taskTemplate = document.getElementById('taskTemplate');
 
 function renderTasks() {
-    clearElement(tasksList);
-    tasks.forEach(task => {
-        const taskElement = document.importNode(taskTemplate.content, true);
-        const checkBox = taskElement.querySelector('input');
-        const label = taskElement.querySelector('label');
+  clearElement(tasksList);
+  tasks.forEach((task) => {
+    const taskElement = document.importNode(taskTemplate.content, true);
+    const checkBox = taskElement.querySelector('input');
+    const label = taskElement.querySelector('label');
 
-        checkBox.id = task.id;
-        checkBox.checked = task.complete;
+    checkBox.id = task.id;
+    checkBox.checked = task.complete;
 
-        label.htmlFor = task.id;
-        label.append(task.task);
-        tasksList.appendChild(taskElement);
-    })
-    save();
+    label.htmlFor = task.id;
+    label.append(task.task);
+    tasksList.appendChild(taskElement);
+  });
+  save();
 }
+
+const taskInput = document.getElementById('addToList');
+taskInput.value = null;
+
+newTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const task = taskInput.value;
+  if (task == null || task === '') return;
+  const newTask = createTask(task);
+  tasks.push(newTask);
+  taskInput.value = null;
+  renderTasks();
+});
+
+tasksList.addEventListener('click', (e) => {
+  const selectedTask = tasks.find((task) => task.id === e.target.id);
+  selectedTask.complete = e.target.checked;
+  save();
+});
 
 const clearButton = document.getElementById('clearButton');
 
-function tst() {
-    clearElement(tasksList);
-    save();
-}
-
-clearButton.addEventListener('click', e => {
-    tasks = tasks.filter(task => !task.complete);
-    save();
-    renderTasks();
+clearButton.addEventListener('click', () => {
+  tasks = tasks.filter((task) => !task.complete);
+  save();
+  renderTasks();
 });
 
 const resetList = document.getElementById('resetList');
-resetList.addEventListener('click', e => {
-    tasks = null;
-    localStorage.clear();
-    renderTasks();
-})
+resetList.addEventListener('click', () => {
+  tasks = null;
+  localStorage.clear();
+  renderTasks();
+});
 
 renderTasks();
--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-        print: './src/print.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
     },
-    module: {
-        rules: [{
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
     ],
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 };
