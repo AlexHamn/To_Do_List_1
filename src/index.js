6834e9d19ed506c5eea9cd5de96e5939d0a36bbc
AlexHamn <agalexandro16@gmail.com>
2021-09-03 17:38:05 -0500

    implement add \/ remove functionality

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import './style.css';
-import clearElement from './statusUpdate';
+import clearElement, {
+  taskInput, tasks, LOCAL_STORAGE_TASKS_KEY,
+  updateStatus, clearCompleted, resetTasks,
+  updateTask, clearOverwrite,
+} from './statusUpdate';
 
 const newTaskForm = document.querySelector('[data-new-task-form]');
 const tasksList = document.getElementById('tasks');
 
-const LOCAL_STORAGE_TASKS_KEY = 'tasks.list';
-let tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASKS_KEY)) || [];
-
-const taskInput = document.getElementById('addToList');
 taskInput.value = null;
 
 function save() {
   localStorage.setItem(LOCAL_STORAGE_TASKS_KEY, JSON.stringify(tasks));
-}
-
-function createTask(task) {
-  return { id: Date.now().toString(), task, complete: false };
 }
 
 const taskTemplate = document.getElementById('taskTemplate');
@@ -25,45 +21,51 @@ function renderTask() {
   tasks.forEach((task) => {
     const taskElement = document.importNode(taskTemplate.content, true);
     const checkBox = taskElement.querySelector('input');
-    const label = taskElement.querySelector('label');
+    const text = taskElement.getElementById('txt');
+    const form = taskElement.getElementById('form');
 
     checkBox.id = task.id;
     checkBox.checked = task.complete;
 
-    label.htmlFor = task.id;
-    label.append(task.task);
+    text.htmlFor = task.id;
+    text.value = (task.task);
+
+    form.id = task.id;
+
+    form.addEventListener('submit', (e) => {
+      task.overwrite = true;
+      updateTask(e);
+      clearOverwrite();
+    });
+    form.addEventListener('submit', renderTask);
+
     tasksList.appendChild(taskElement);
   });
   save();
 }
 
-newTaskForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const task = taskInput.value;
-  if (task == null || task === '') return;
-  const newTask = createTask(task);
-  tasks.push(newTask);
-  taskInput.value = null;
-  renderTask();
-});
+newTaskForm.addEventListener('submit', updateStatus);
+newTaskForm.addEventListener('submit', renderTask);
 
 tasksList.addEventListener('click', (e) => {
   const selectedTask = tasks.find((task) => task.id === e.target.id);
-  selectedTask.complete = e.target.checked;
-  save();
+  if (selectedTask) {
+    selectedTask.complete = e.target.checked;
+    save();
+  }
 });
 
 const clearButton = document.getElementById('clearButton');
 
 clearButton.addEventListener('click', () => {
-  tasks = tasks.filter((task) => !task.complete);
+  clearCompleted();
   save();
   renderTask();
 });
 
 const resetList = document.getElementById('resetList');
 resetList.addEventListener('click', () => {
-  tasks = null;
+  resetTasks();
   localStorage.clear();
   renderTask();
 });
--- src/statusUpdate.js
+++ src/statusUpdate.js
@@ -3,3 +3,44 @@ export default function clearElement(element) {
     element.removeChild(element.firstChild);
   }
 }
+
+export const taskInput = document.getElementById('addToList');
+export const LOCAL_STORAGE_TASKS_KEY = 'tasks.list';
+/* eslint-disable import/no-mutable-exports */
+export let tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASKS_KEY)) || [];
+
+export function createTask(task) {
+  return {
+    id: Date.now().toString(), task, complete: false, overwrite: false,
+  };
+}
+
+export const updateStatus = (e) => {
+  e.preventDefault();
+  const task = taskInput.value;
+  if (task == null || task === '') return;
+  const newTask = createTask(task);
+  tasks.push(newTask);
+  taskInput.value = null;
+};
+
+export const updateTask = (e) => {
+  const task = e.target.children[0].value;
+  tasks = tasks.filter((task) => task);
+  if (task !== null || task !== '') {
+    const newTask = createTask(task);
+    tasks.push(newTask);
+  }
+};
+
+export function clearCompleted() {
+  tasks = tasks.filter((task) => !task.complete);
+}
+
+export function clearOverwrite() {
+  tasks = tasks.filter((task) => !task.overwrite);
+}
+
+export function resetTasks() {
+  tasks = [];
+}
