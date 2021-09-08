790ea424b115f80aa534a0d92c3485eb50b4d59d
AlexHamn <agalexandro16@gmail.com>
2021-09-08 16:31:10 -0500

    change the mname of the dist folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
 };
