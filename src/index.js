1e24b96ff1ebe0456b0f849e4eff2409b3597b9f
AlexHamn <agalexandro16@gmail.com>
2021-09-28 15:37:53 -0500

    make meaningless change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './style.css';
 import clearElement, {
-  taskInput, tasks, LOCAL_STORAGE_TASKS_KEY,
-  updateStatus, clearCompleted, resetTasks,
-  updateTask, clearOverwrite,
+    taskInput,
+    tasks,
+    LOCAL_STORAGE_TASKS_KEY,
+    updateStatus,
+    clearCompleted,
+    resetTasks,
+    updateTask,
+    clearOverwrite,
 } from './statusUpdate';
 
 const newTaskForm = document.querySelector('[data-new-task-form]');
@@ -11,63 +16,65 @@ const tasksList = document.getElementById('tasks');
 taskInput.value = null;
 
 function save() {
-  localStorage.setItem(LOCAL_STORAGE_TASKS_KEY, JSON.stringify(tasks));
+    localStorage.setItem(LOCAL_STORAGE_TASKS_KEY, JSON.stringify(tasks));
 }
 
 const taskTemplate = document.getElementById('taskTemplate');
 
 function renderTask() {
-  clearElement(tasksList);
-  tasks.forEach((task) => {
-    const taskElement = document.importNode(taskTemplate.content, true);
-    const checkBox = taskElement.querySelector('input');
-    const text = taskElement.getElementById('txt');
-    const form = taskElement.getElementById('form');
+    clearElement(tasksList);
+    tasks.forEach((task) => {
+        const taskElement = document.importNode(taskTemplate.content, true);
+        const checkBox = taskElement.querySelector('input');
+        const text = taskElement.getElementById('txt');
+        const form = taskElement.getElementById('form');
 
-    checkBox.id = task.id;
-    checkBox.checked = task.complete;
+        checkBox.id = task.id;
+        checkBox.checked = task.complete;
 
-    text.htmlFor = task.id;
-    text.value = (task.task);
+        text.htmlFor = task.id;
+        text.value = (task.task);
 
-    form.id = task.id;
+        form.id = task.id;
 
-    form.addEventListener('submit', (e) => {
-      task.overwrite = true;
-      updateTask(e);
-      clearOverwrite();
+        form.addEventListener('submit', (e) => {
+            task.overwrite = true;
+            updateTask(e);
+            clearOverwrite();
+        });
+        form.addEventListener('submit', renderTask);
+
+        tasksList.appendChild(taskElement);
     });
-    form.addEventListener('submit', renderTask);
-
-    tasksList.appendChild(taskElement);
-  });
-  save();
+    save();
 }
 
 newTaskForm.addEventListener('submit', updateStatus);
 newTaskForm.addEventListener('submit', renderTask);
 
 tasksList.addEventListener('click', (e) => {
-  const selectedTask = tasks.find((task) => task.id === e.target.id);
-  if (selectedTask) {
-    selectedTask.complete = e.target.checked;
-    save();
-  }
+    const selectedTask = tasks.find((task) => task.id === e.target.id);
+    if (selectedTask) {
+        selectedTask.complete = e.target.checked;
+        save();
+    }
 });
 
 const clearButton = document.getElementById('clearButton');
 
 clearButton.addEventListener('click', () => {
-  clearCompleted();
-  save();
-  renderTask();
+    clearCompleted();
+    save();
+    renderTask();
 });
 
 const resetList = document.getElementById('resetList');
 resetList.addEventListener('click', () => {
-  resetTasks();
-  localStorage.clear();
-  renderTask();
+    resetTasks();
+    localStorage.clear();
+    renderTask();
 });
 
 renderTask();
+
+// hrigks
